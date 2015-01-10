1a95395e76f7ca99af7f9a996e403fe7b552228b
Kiran Kurian <Kiran@Kirans-MBP.home>
2015-01-09 22:47:15 -0500

    Added multi-classes

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2012 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-"""Starting template for Google App Engine applications.
-
-Use this project as a starting point if you are just beginning to build a Google
-App Engine project. Remember to download the OAuth 2.0 client secrets which can
-be obtained from the Developer Console <https://code.google.com/apis/console/>
-and save them as 'client_secrets.json' in the project directory.
-"""
-
 import logging
 import os
 import pickle
@@ -54,15 +29,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-
-# CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
-# application, including client_id and client_secret.
-# You can see the Client ID and Client secret on the API Access tab on the
-# Google APIs Console <https://code.google.com/apis/console>
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
-# Helpful message to display in the browser if the CLIENT_SECRETS file
-# is missing.
 MISSING_CLIENT_SECRETS_MESSAGE = """
 <h1>Warning: Please configure OAuth 2.0</h1>
 <p>
@@ -77,13 +45,6 @@ href="https://code.google.com/apis/console">APIs Console</a>.
 
 """ % CLIENT_SECRETS
 
-
-
-
-# Set up an OAuth2Decorator object to be used for authentication.  Add one or
-# more of the following scopes in the scopes parameter below. PLEASE ONLY ADD
-# THE SCOPES YOU NEED. For more information on using scopes please see
-# <https://developers.google.com/+/best-practices>.
 decorator = OAuth2DecoratorFromClientSecrets(
     CLIENT_SECRETS,
     scope='https://www.googleapis.com/auth/calendar',
@@ -99,9 +60,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         test = ""
         page_token = None
-#example from calendarsList
         newClass=True
-        # baseURL=self.request.url
         template = JINJA_ENVIRONMENT.get_template('main.html')
         self.response.write(template.render())
 class addEvent(webapp2.RequestHandler):
@@ -109,114 +68,128 @@ class addEvent(webapp2.RequestHandler):
     def post(self):
         template = JINJA_ENVIRONMENT.get_template('addClass.html')
         self.response.write(template.render())
-        if decorator.has_credentials(): 
-            # summary=cgi.escape(self.request.get('coursetitle'))
-            try: 
-              subjectNumer=cgi.escape(self.request.get('subjectNumer'))
-              courseNumber=cgi.escape(self.request.get('courseNumber'))
-              sectionNumber=cgi.escape(self.request.get('sectionNumber'))
-
-              cInfo=courseInfo(subjectNumer,courseNumber,sectionNumber)
-              
-              locations=cInfo[0]
-              startTimes=cInfo[1]
-              endTimes=cInfo[2]
-              days=cInfo[3]
-              summary=cInfo[4]
-
-              errorCheck=True
-
-              for index in range(len(locations)):
-                  day="%s"%(days[index]).lower()
-                  if day=="monday" or day=="m":
-                      startDate="2015-01-26"
-                  elif day=="tuesday" or day=="t":
-                      startDate="2015-01-20"
-                  elif day=="wednesday" or day=="w":
-                      startDate="2015-01-21"
-                  elif day=="thursday" or day=="th":
-                      startDate="2015-01-22"
-                  elif day=="friday" or day=="f":
-                      startDate="2015-01-23"
-                  else:
-                      self.response.out.write("Couldn't recognize day: %s "%(day))
-                  startTime="%s%s"%(startTimes[index],":00")
-                  endTime="%s%s"%(endTimes[index],":00")
-                  location="%s"%(locations[index])
+        if decorator.has_credentials():
+            errorCheck=None
+            try:
+              print "start"
+              errorCheck=":)" 
+              subjectNumber=[]
+              if self.request.get('subjectNumber1'):
+                subjectNumber.append(self.request.get('subjectNumber1'))
+              if self.request.get('subjectNumber5'):
+                subjectNumber.append(self.request.get('subjectNumber5'))
+              if self.request.get('subjectNumber2'):
+                subjectNumber.append(self.request.get('subjectNumber2'))
+              if self.request.get('subjectNumber3'):
+                subjectNumber.append(self.request.get('subjectNumber3'))
+              if self.request.get('subjectNumber4'):
+                subjectNumber.append(self.request.get('subjectNumber4'))
+              courseNumber=[self.request.get('courseNumber1'),self.request.get('courseNumber2'),self.request.get('courseNumber3'),self.request.get('courseNumber4'),self.request.get('courseNumber5')]
+              sectionNumber=[self.request.get('sectionNumber1'),self.request.get('sectionNumber2'),self.request.get('sectionNumber3'),self.request.get('sectionNumber4'),self.request.get('sectionNumber5')]
+              print subjectNumber
+              for classIndex in range(len(subjectNumber)):
+                print "...what're you lookin' at?"
+                cInfo=courseInfo(subjectNumber[classIndex],courseNumber[classIndex],sectionNumber[classIndex])
                 
-                  event = {
-                  "location": "%s"%(location),
-                   "end": {
-                       "dateTime": "%sT%s"%(startDate,endTime),
-                      "timeZone": "America/New_York"
-                   },
-                   "start": {
-                       "dateTime": "%sT%s"%(startDate,startTime),
-                      "timeZone": "America/New_York"
-                   },
-                   "summary": summary,
-                   "recurrence": [
-                    'RRULE:FREQ=WEEKLY;UNTIL=20150505T000000Z',
-                   ],
-                   "reminders": {
-                      "useDefault":"false",
-                      "overrides": [
-                      {
-                          "method":"popup",
-                          "minutes": 20
-                       }
-                      ]
-                  }
-                  }
-                  reminder=cgi.escape(self.request.get('reminder'))
-                  if reminder=="reminder-none":
-                    event["reminders"] = {
-                        "useDefault":"false",
-                        "overrides": [
-                        ]
-                    }
-                  elif reminder=='reminder-40':
-                    event["reminders"] = {
-                        "useDefault":"false",
-                        "overrides": [
-                        {
-                          "method":"popup",
-                          "minutes": 40
-                        }
-                        ]
-                    }
-                  elif reminder=='reminder-60':
-                    event["reminders"] = {
-                        "useDefault":"false",
-                        "overrides": [
-                        {
-                          "method":"popup",
-                          "minutes": 60
-                        }
-                        ]
-                    }
-                  http = decorator.http()
+                locations=cInfo[0]
+                startTimes=cInfo[1]
+                endTimes=cInfo[2]
+                days=cInfo[3]
+                summary=cInfo[4]
 
-                  recurring_event = service.events().insert(calendarId='primary', body=event).execute(http=http)
+                for index in range(len(locations)):
+                    day="%s"%(days[index]).lower()
+                    if day=="monday" or day=="m":
+                        startDate="2015-01-26"
+                    elif day=="tuesday" or day=="t":
+                        startDate="2015-01-20"
+                    elif day=="wednesday" or day=="w":
+                        startDate="2015-01-21"
+                    elif day=="thursday" or day=="th":
+                        startDate="2015-01-22"
+                    elif day=="friday" or day=="f":
+                        startDate="2015-01-23"
+                    else:
+                        self.response.out.write("Couldn't recognize day: %s "%(day))
+                    startTime="%s%s"%(startTimes[index],":00")
+                    endTime="%s%s"%(endTimes[index],":00")
+                    location="%s"%(locations[index])
+                  
+                    event = {
+                    "location": "%s"%(location),
+                     "end": {
+                         "dateTime": "%sT%s"%(startDate,endTime),
+                        "timeZone": "America/New_York"
+                     },
+                     "start": {
+                         "dateTime": "%sT%s"%(startDate,startTime),
+                        "timeZone": "America/New_York"
+                     },
+                     "summary": summary,
+                     "recurrence": [
+                      'RRULE:FREQ=WEEKLY;UNTIL=20150505T000000Z',
+                     ],
+                     "reminders": {
+                        "useDefault":"false",
+                        "overrides": [
+                        {
+                            "method":"popup",
+                            "minutes": 20
+                         }
+                        ]
+                    }
+                    }
+                    reminder=self.request.get('reminder')
+                    if reminder=="reminder-none":
+                      event["reminders"] = {
+                          "useDefault":"false",
+                          "overrides": [
+                          ]
+                      }
+                    elif reminder=='reminder-40':
+                      event["reminders"] = {
+                          "useDefault":"false",
+                          "overrides": [
+                          {
+                            "method":"popup",
+                            "minutes": 40
+                          }
+                          ]
+                      }
+                    elif reminder=='reminder-60':
+                      event["reminders"] = {
+                          "useDefault":"false",
+                          "overrides": [
+                          {
+                            "method":"popup",
+                            "minutes": 60
+                          }
+                          ]
+                      }
+                    http = decorator.http()
+
+                    recurring_event = service.events().insert(calendarId='primary', body=event).execute(http=http)
+                    print "sucess"
             except:
-                errorCheck=False
+              errorCheck="WTF"
+              print "fail"
+            if errorCheck==":)":
+              self.response.out.write("<center><h2>Awesome, you class was added to your schedule!</h2></center>")
+            else:
+              self.response.out.write("<center>Oops, ran into an error when trying to add your class to your calendar. Try again, you may have mistyped your class info</center>")
+            self.response.out.write("""<div class="row uniform 50%">
+            <div class="6u 12u(3)"><center>
+              <form action=/>
+                <input type="submit" value="Add More Classes">
+              </form></center>
+            </div>
+            <div class="6u 12u(3)"><center>
+              <form action=https://www.google.com/calendar/>
+                <input type="submit" value="Go to Calendar">
+              </form></center>
+            </div></div>""")
         else:
             self.response.out.write("Error, no credentials")
-        if errorCheck:
-          self.response.out.write("<center><h2>Awesome, you class was added to your schedule!</h2></center>")
-        else:
-          self.response.out.write("<center>Oops, ran into an error when trying to add your class to your calendar. Try again, you may have mistyped your class info</center>")
-        self.response.out.write("""<div class="row uniform 50%">
-        <div class="6u 12u(3)"><center>
-          <form action=/>
-            <input type="submit" value="Add Another Class">
-          </form></center>
-        </div>
-        <div class="6u 12u(3)"><center>
-          <form action=https://www.google.com/calendar/>
-            <input type="submit" value="Go to Calendar">
-          </form></center>
-        </div></div>""")
 
 
 application = webapp.WSGIApplication(
