82ae5aba324547ab96eb9ca26f016e5a9a64d626
Kiran Kurian <kiran.kurian@rutgers.edu>
2016-08-10 19:25:19 -0400

    Removed test

--- test/clearDB.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(port=27106)
-db=client.spring16
-db.brothers.remove()
--- test/showDB.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(port=27106)
-db=client.spring16
-
-cursor=db.brothers.find()
-for document in cursor:
-	print document
