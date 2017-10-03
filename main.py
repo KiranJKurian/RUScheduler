d687bc96e2032d4891644a3d689bb46c7d937c03
Kiran Kurian <kiran.kurian@rutgers.edu>
2017-10-03 19:19:15 -0400

    Added calendarId to DB

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def classes(http_auth, inputDict, calendarId = "primary", preText = "", postText
 				service.events().update(calendarId=calendarId, eventId=instance['id'], body=instance).execute()
 
 	# db.scheduler.remove()
-	insertRecordAsync({"name": userInfo['name'],"email": userInfo['email'], "success": summary, "error": None})
+	insertRecordAsync({"name": userInfo['name'],"email": userInfo['email'], "success": summary, "error": None, "calendarId": calendarId})
 	return {"success":True,"course": summary}
 
 # Group Classes
